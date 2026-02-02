af6c3b6ca4c65905d001adf9594f69f5527e0b32
AminurAlam <64137875+AminurAlam@users.noreply.github.com>
2026-02-02 08:22:19 +0530

    spot-image: skip showing section instead of crashing

--- spot-image.yazi/main.lua
+++ spot-image.yazi/main.lua
@@ -3,22 +3,22 @@ local M = {}
 -- TODO: cache json
 
 ---@param job Job
----@return Sections?
+---@return Sections
 local image_info = function(job)
   local output, err = Command('magick'):arg({ tostring(job.file.url), 'json:' }):output()
 
   if not output or err then
-    ya.err('Failed to start `magick`:', err)
-    return
+    ya.err('Failed to start `magick`: ', tostring(err))
+    return {}
   elseif not output.status.success then
     ya.err(output.stderr)
-    return
+    return {}
   end
 
   local t = ya.json_decode(output.stdout)
   if not t or type(t) ~= 'table' then
     ya.err('Bad `magick` output:', output.stdout)
-    return
+    return {}
   end
 
   local img = t[1].image
@@ -59,9 +59,11 @@ local image_info = function(job)
 
   for key, value in pairs(properties) do
     --  TODO: add filter
-    if string.match(key, '^exif:.+') then
-      exif[#exif + 1] = { string.sub(key, 6, #key), tostring(value) }
+    if not string.match(key, '^exif:.+') then
+      goto continue
     end
+    exif[#exif + 1] = { string.sub(key, 6, #key), tostring(value) }
+    ::continue::
   end
 
   ya.dbg(#exif, exif)
@@ -75,12 +77,12 @@ end
 
 ---@param job Job
 function M:spot(job)
-  local sections = image_info(job) ---@type Sections?
+  local sections = image_info(job) ---@type Sections
   local opts
   if #sections == 2 then
     opts = { style = { key_length = 25 } }
   end
-  require('spot'):spot(job, sections or {}, opts)
+  require('spot'):spot(job, sections, opts)
 end
 
 return M
