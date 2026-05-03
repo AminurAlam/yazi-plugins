ced5d4c242c3b69d24ecd5f69b68d583f066ac0b
Harshit-Dhanwalkar <harshitpd1729@gmail.com>
2026-05-03 11:08:42 +0530

    preview-git: add colorized  git log output

--- preview-git.yazi/main.lua
+++ preview-git.yazi/main.lua
@@ -1,24 +1,39 @@
+---@diagnostic disable: undefined-global
+
 local M = {}
 
--- TODO: colorized outpt
+-- TODO: colorized output (current only git log)
 -- TODO: show all remotes
--- TODO: show all stash
 -- TODO: show all branches
+-- TODO: show all stash
 function M:peek(job)
-  local ori = Command('git'):arg({ 'remote', 'get-url', 'origin' }):output()
-  local ups = Command('git'):arg({ 'remote', 'get-url', 'upstream' }):output()
-  local sts = Command('git'):arg({ '--no-optional-locks', 'status', '-bs' }):output()
-  local log = Command('git'):arg({ 'log', '--format=%s', '-n15' }):output()
+	local ori = Command("git"):arg({ "remote", "get-url", "origin" }):output()
+	local ups = Command("git"):arg({ "remote", "get-url", "upstream" }):output()
+	local remotes = Command("git"):arg({ "remote", "-v" }):output()
+	local status = Command("git"):arg({ "--no-optional-locks", "status", "-bs" }):output()
 
-  local text = ''
-    .. (ori and ori.stdout ~= '' and ('origin: ' .. ori.stdout) or '')
-    .. (ups and ups.stdout ~= '' and ('upstream: ' .. ups.stdout) or '')
-    .. '\n'
-    .. (sts and sts.stdout)
-    .. '\n'
-    .. (log and log.stdout)
+	local log = Command("git")
+		:arg("-c")
+		:arg("color.ui=always")
+		:arg("log")
+		:arg("--graph")
+		:arg("--format=%C(auto)%h %s %C(magenta)(%cr)%C(auto)%d")
+		:arg("-n15")
+		:output()
 
-  ya.preview_widget(job, ui.Text.parse(text):area(job.area))
+	local text = ""
+		.. (ori and ori.stdout ~= "" and ("origin: " .. ori.stdout) or "")
+		.. (ups and ups.stdout ~= "" and ("upstream: " .. ups.stdout) or "")
+		.. "\n"
+		.. "Remotes:\n"
+		.. (remotes and remotes.stdout)
+		.. "\n"
+		.. (status and status.stdout ~= "" and ("status: " .. status.stdout) or "")
+		.. "\n"
+		.. "Log:\n"
+		.. (log and log.stdout)
+
+	ya.preview_widget(job, ui.Text.parse(text):area(job.area))
 end
 
 function M:seek() end
